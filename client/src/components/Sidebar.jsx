b4eed5f5e88679a1c179ec0b8779732a08273ca7
harsh661 <plasmaharsh@gmail.com>
2023-06-03 14:05:02 +0530

    Fixed heigth of sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = ({ shown }) => {
     <div
       className={`h-responsive_height p-3 ${
         shown ? "block" : "hidden lg:block"
-      } animate-slide-right group relative lg:absolute xl:relative h-screen z-10`}
+      } animate-slide-right group relative lg:absolute xl:relative z-10`}
     >
       <div className="bg-lighter xl:bg-transparent rounded-2xl w-max h-full flex flex-col justify-between items-center p-2 transition-all duration-200">
         <div className="flex flex-col gap-5 h-full">
